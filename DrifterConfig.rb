fa7b720ef24442273b04524e1b9034ced6057339
Miles Whittaker <mjwhitta@gmail.com>
2017-02-26 18:35:40 -0500

    Deleted boxes from DriferConfig.rb
    These boxes were old or don't really matter.

--- DrifterConfig.rb
+++ DrifterConfig.rb
@@ -4,20 +4,6 @@ module DrifterConfig
     def self.get()
         boxes = Array.new
 
-        # Create Manjaro box
-        # manjaro = Box.new("boxes/manjaro-openbox-0.8.10-x86_64.box",
-        #                   "manjaro")
-        # manjaro.cpus = "2"
-        # manjaro.memory = "2048"
-        # boxes.push(manjaro)
-
-        # Create Kali box
-        kali = Box.new("mjwhitta/kali-linux-1.0.7-amd64", "kali")
-        kali.cpus = "2"
-        kali.memory = "2048"
-        kali.username = "root"
-        boxes.push(kali)
-
         # Create Nebula box
         nebula = Box.new("mjwhitta/exploit-exercises-nebula-5",
                          "nebula")
@@ -47,16 +33,6 @@ module DrifterConfig
         fusion.set_iso_only
         fusion.username = "fusion"
         boxes.push(fusion)
-
-        # Create DevStack box
-        devstack = Box.new("mjwhitta/openstack-dev-devstack",
-                           "devstack")
-        devstack.cpus = "2"
-        devstack.forward_ports[80] = 8001
-        devstack.memory = "2048"
-        devstack.scripts.clear
-        devstack.username = "user"
-        boxes.push(devstack)
 
         # Create WebGoat box
         webgoat = Box.new("mjwhitta/owasp-webgoat", "webgoat")
